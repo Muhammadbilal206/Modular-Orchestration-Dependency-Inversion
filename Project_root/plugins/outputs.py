4dcc8b3c5f78715d14c48dbab8dc172788efc463
usmankchaudhary24 <usmankchaudhary24@gmail.com>
2026-03-01 13:46:55 +0500

    Update outputs.py
    changed visuals

--- Project_root/plugins/outputs.py
+++ Project_root/plugins/outputs.py
@@ -26,29 +26,49 @@ class GraphicsChartWriter:
 
             if metric in ["Top 10 Countries", "Bottom 10 Countries"]:
                 fig = px.bar(data, x="Country", y="GDP", title=metric, template="plotly_dark", 
-                             color="GDP", color_continuous_scale="Sunsetdark", text_auto=".2s")
-                fig.update_layout(xaxis={'categoryorder':'total descending'})
+                             color="GDP", color_continuous_scale="Sunsetdark", text_auto=".3s")
+                
+                fig.update_layout(
+                    xaxis={'categoryorder':'total descending'},
+                    xaxis_tickangle=-45,
+                    coloraxis_showscale=False,
+                    margin=dict(b=120)
+                )
+                fig.update_traces(textposition='outside', cliponaxis=False)
                 fig.show()
 
             elif metric == "Growth Rates":
+                data = sorted(data, key=lambda x: x["Growth"], reverse=True)
+                
                 fig = px.bar(data, x="Country", y="Growth", title=metric, template="plotly_dark", 
-                             color="Growth", color_continuous_scale="Tealrose", text_auto=".2f")
+                             color="Growth", color_continuous_scale="Tealrose", text_auto=".1f")
+                
+                fig.update_layout(
+                    xaxis_tickangle=-45,
+                    coloraxis_showscale=False,
+                    margin=dict(b=150, t=50, l=50, r=50),
+                    uniformtext_minsize=9, 
+                    uniformtext_mode='hide'
+                )
+                fig.update_traces(textposition='outside', cliponaxis=False)
                 fig.show()
 
             elif metric == "Avg GDP Continent":
                 fig = px.line(data, x="Year", y="Average_GDP", title=metric, template="plotly_dark", 
                               markers=True, line_shape="spline")
-                fig.update_traces(line=dict(color="cyan", width=3), marker=dict(size=8))
+                fig.update_traces(line=dict(color="#00F0FF", width=4), marker=dict(size=10, color="white"))
+                fig.update_layout(margin=dict(b=50))
                 fig.show()
 
             elif metric == "Global Trend":
                 fig = px.line(data, x="Year", y="Total_GDP", title=metric, template="plotly_dark", 
                               markers=True, line_shape="spline")
-                fig.update_traces(line=dict(color="magenta", width=3), marker=dict(size=8))
+                fig.update_traces(line=dict(color="#FF00FF", width=4), marker=dict(size=10, color="white"))
+                fig.update_layout(margin=dict(b=50))
                 fig.show()
 
             elif metric == "Continent Contributions":
                 fig = px.pie(data, names="Continent", values="Contribution_Percent", title=metric, 
                              template="plotly_dark", color_discrete_sequence=px.colors.qualitative.Pastel)
-                fig.update_traces(textposition='inside', textinfo='percent+label')
+                fig.update_traces(textposition='inside', textinfo='percent+label', pull=[0.05]*len(data))
                 fig.show()
